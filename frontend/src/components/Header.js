7da4c8d40e9799b7311c2cc5f2b199ea2f164039
Donald Nyingifa <donaldnyingifa@gmail.com>
2021-04-21 21:16:28 +0100

    added mongoose, formatted some code on the frontend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import {LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
-    return (
-        <header>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container>
-                    <LinkContainer to='/'>
-                        <Navbar.Brand>Lab Cosmetics</Navbar.Brand>
-                    </LinkContainer>
-                    
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ml-auto">
-                        <LinkContainer to='/cart'>
-                            <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to='/login'>
-                            <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
-                        </LinkContainer>
-                        
-                        
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-</Navbar>
-        </header>
-    )
-}
+  return (
+    <header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Lab Cosmetics</Navbar.Brand>
+          </LinkContainer>
 
-export default Header
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
