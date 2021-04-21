846078eb08e5d654293a7e32595ab31019ac7569
Donald Nyingifa <donaldnyingifa@gmail.com>
2021-04-21 10:25:09 +0100

    added more components - Product and Rating

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,29 @@
 import React from 'react'
+import {LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-  <Navbar.Brand href="/">Lab Cosmetics</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-      <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
-      
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Lab Cosmetics</Navbar.Brand>
+                    </LinkContainer>
+                    
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                        <LinkContainer to='/cart'>
+                            <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+                        </LinkContainer>
+                        
+                        
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
 </Navbar>
         </header>
     )
