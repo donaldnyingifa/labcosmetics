5d7d21ec8baf9f3970785ef1408ef8292aff61c1
Donald Nyingifa <donaldnyingifa@gmail.com>
2021-06-29 21:54:37 +0100

    added backend authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js';
 import User from "../models/userModel.js";
 
 // @desc Auth user & get token
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null,
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
@@ -24,6 +25,67 @@ const authUser = asyncHandler(async (req, res) => {
 
     res.send({ email, password })
 })
+
+// @desc Fetch all profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+})
+
+
+
+
+
+
+
+
 
 // @desc Fetch all users
 // @route GET /api/users
@@ -36,7 +98,7 @@ const getUsers = asyncHandler(async (req, res) => {
 
 // @desc Fetch single user
 // @route GET /api/users/:id
-// @access Public
+// @access Private
 const getUserById = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
 
@@ -49,4 +111,4 @@ const getUserById = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUsers, getUserById }
+export { authUser, registerUser, getUserProfile, getUsers, getUserById }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUsers, getUserById } from '../controllers/userController.js'
+import { authUser, registerUser, getUsers, getUserById, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile)
 
 router.route("/").get(getUsers);
 router.route('/:id').get(getUserById);
-router.post("/login", authUser);
 
 export default router;
